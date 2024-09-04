02bcfc60bfaf9fd78e087c771edc68523fd1c711
Ethan Haque <eth.haque@gmail.com>
2024-09-04 11:38:59 -0700

    add basic sentence generation, sentence templates and word pools

--- controllers/sentences.js
+++ controllers/sentences.js
@@ -1,3 +1,30 @@
+const fs = require('fs');
+
+// Load word pools and sentence templates from JSON
+const wordPools = JSON.parse(fs.readFileSync('./data/wordPools.json', 'utf8'));
+const sentenceTemplates = JSON.parse(fs.readFileSync('./data/sentenceTemplates.json', 'utf8')).templates;
+
 exports.getSentences = (req, res, next) => {
-    res.send(req.params.amount + ' sentences.');
+  const sentences = [];
+  for (let i = 0; i < req.params.amount; i++) {
+    sentences.push(generateRandomSentence());
+  }
+  res.send(sentences);
 };
+
+// return a random word from an array
+function getRandomWord(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+// return a random sentence generated from a template
+function generateRandomSentence() {
+  const template = getRandomWord(sentenceTemplates);
+
+  return template
+    .replace(/\{noun\}/g, getRandomWord(wordPools.nouns))
+    .replace(/\{verb\}/g, getRandomWord(wordPools.verbs))
+    .replace(/\{adjective\}/g, getRandomWord(wordPools.adjectives))
+    .replace(/\{adverb\}/g, getRandomWord(wordPools.adverbs))
+    .replace(/\{preposition\}/g, getRandomWord(wordPools.prepositions));
+}
