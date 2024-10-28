feb804d9b7557ad0de64373f3a6fd18325cf6525
Ethan Haque <eth.haque@gmail.com>
2024-10-28 11:37:41 -0700

    remove api key auth, add session auth

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+const pool = require('./db');  // Database pool setup
 const bodyParser = require('body-parser');
 const cors = require('cors');
 let sentenceRouter = require('./routes/sentences');
+let userRouter = require('./routes/user');
 require('dotenv').config()
 
 const apiKey = process.env.API_KEY;
@@ -14,17 +18,19 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-function checkApiKey(req, res, next) {
-  const clientApiKey = req.headers['x-api-key'];
-  if (clientApiKey && clientApiKey === apiKey) {
-    next();
-  } else {
-    res.status(401).json({ message: 'Unauthorized' });
-  }
-}
+// Session configuration
+app.use(session({
+  store: new pgSession({
+      pool,  // PostgreSQL pool
+  }),
+  secret: process.env.SESSION_SECRET,  // Secret for signing cookies
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false, httpOnly: true, maxAge: 1000 * 60 * 60 * 24 }  // 1 day
+}));
 
-app.use(checkApiKey); // Apply API key check globally
 app.use('/sentences', sentenceRouter);
+app.use('/user', userRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 module.exports = app;
